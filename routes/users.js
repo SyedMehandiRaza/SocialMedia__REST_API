7599456f0b5d72e1aceeae72dc7914eedceda697
Syed Mehandi Raza <mehandixaidi786@gmail.com>
2024-02-16 02:24:26 +0530

    added delete and update user

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,49 @@
 const router = require('express').Router()
+const User = require('../models/User.model');
+const bcrypt = require('bcrypt');
 
-router.get("/",(req,res) => {
-  res.send("HomePage")
+// update user
+
+router.put("/:id", async(req,res) => {
+  if(req.body.userId ===req.params.id || req.body.isAdmin){
+    if(req.body.password)
+
+      try{
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt)
+      }catch(err){
+        return res.status(500).json(err)
+      }
+      try {
+        const user = await User.findByIdAndUpdate(req.params.id , {
+          $set:req.body
+        });
+        res.status(200),json("Account has been updated")
+      } catch (error) {
+        
+      }
+  }else{
+    return res.status(403).json("You can update only your account")
+  }
 })
+
+// delete user
+router.delete("/:id", async(req,res) => {
+  if(req.body.userId ===req.params.id || req.body.isAdmin){
+    
+      try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        res.status(200),json("Account has been delete successfully")
+      } catch (error) {
+        
+      }
+  }else{
+    return res.status(403).json("You can delete only your account")
+  }
+})
+// get user
+// follow user
+// unfollow user
+
 
 module.exports = router
